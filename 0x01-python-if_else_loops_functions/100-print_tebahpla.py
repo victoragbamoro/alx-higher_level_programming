fa786ddb6ae2e263641eef1c81037ce9b39ef120
Victor Agbamoro <agbamorovictor@gmail.com>
2022-06-24 09:11:05 +0000

    changes

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c + (ord('A') - ord('a'))) if c % 2 else c), end='')  
---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square :
+    """
+    This class represent a square with an empty class
+    """ 
+    pass
